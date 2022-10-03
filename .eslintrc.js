3f40ee0d246af96f1eb883b6c6aaef260ae0b3d4
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-10-03 13:54:23 +0200

    🎉 Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const config = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
+export default config;
